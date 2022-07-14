71d206ad3cd7e2b39bc64513e4fbbcd213e821c8
olivier coussieu <olivier.coussieu@orange.fr>
2022-07-14 11:33:31 +0200

    readbuffer

--- src/main/java/ocd/iramuteq/hachoir/service/FirstHachoirService.java
+++ src/main/java/ocd/iramuteq/hachoir/service/FirstHachoirService.java
@@ -5,17 +5,26 @@ import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import ocd.iramuteq.hachoir.HachoirCommandLineRunner;
 
 @Service
 public class FirstHachoirService implements HachoirService {
 
+    private static Logger LOG = LoggerFactory.getLogger(FirstHachoirService.class);
+	public static final int BUFFER_LEN = 1024;
+	
 	@Override
 	public int write(String source, String destination) throws IOException {
 		System.out.println("@@@ Hachoir. started");
+		int count = 0;
 		try (
 				InputStream is = 
 					new BufferedInputStream(Files.newInputStream(Paths.get(source)));
@@ -23,8 +32,18 @@ public class FirstHachoirService implements HachoirService {
 					new BufferedOutputStream(Files.newOutputStream(Paths.get(destination)))
 				)
 			{
-				is.transferTo(os);
+				byte[] buffer = new byte[BUFFER_LEN];
+				for (int i = is.read(buffer); i >= 0; i = is.read(buffer)) {
+					count += hacher(buffer, os);					
+				}
 			}
+		return count;
+	}
+
+	protected int hacher(byte[] buffer, OutputStream os) {
+		
+	    String contenu = new String(buffer, StandardCharsets.UTF_8);
+		LOG.info(contenu);
 		return 0;
 	}
 
