31026f3dfa7acb6cd195786cc21288536112d044
olivier coussieu <olivier.coussieu@orange.fr>
2023-01-02 10:26:40 +0100

    junit manuel

--- src/test/java/ocd/iramuteq/hachoir/service/PhraseTest.java
+++ src/test/java/ocd/iramuteq/hachoir/service/PhraseTest.java
@@ -8,43 +8,54 @@ import org.junit.jupiter.api.Test;
 
 public class PhraseTest {
 
-    @Test
-    public void testAdd() throws IOException {
-        StringWriter sw = new StringWriter();
-        BufferedWriter bw = new BufferedWriter(sw);
-        Phrase phrase = new Phrase(bw);
-
+	private static final String INJECTION = " \r\n**** *var1_mod0\r\n";
+	StringWriter sw = new StringWriter();
+	BufferedWriter bw = new BufferedWriter(sw);
+	Phrase phrase = new Phrase(bw);
+	
+   @Test
+    public void testAddEmpty() throws IOException {
         // Test avec une ligne vide
         int output = phrase.add("");
         assertEquals(0, output);
 
+
+   }
+    
+    @Test
+    public void testAdd_Line() throws IOException {
+        final String expected = INJECTION;
         // Test avec une ligne qui ne contient pas de point ni d'exclamation
-        output = phrase.add("Ceci est une phrase simple");
+    	int output = phrase.add("Ceci est une phrase simple");
         assertEquals(0, output);
-        assertEquals("Ceci est une phrase simple", sw.toString());
-
-        // Test avec une ligne qui contient un point
-        sw = new StringWriter();
-        bw = new BufferedWriter(sw);
-        phrase = new Phrase(bw);
-        output = phrase.add("Ceci est une phrase simple.");
-        assertEquals(1, output);
-        assertEquals("Ceci est une phrase simple.", sw.toString());
-
-        // Test avec une ligne qui contient une exclamation
-        sw = new StringWriter();
-        bw = new BufferedWriter(sw);
-        phrase = new Phrase(bw);
-        output = phrase.add("Ceci est une phrase simple!");
-        assertEquals(1, output);
-        assertEquals("Ceci est une phrase simple!", sw.toString());
-
-        // Test avec une ligne qui contient plusieurs phrases
-        sw = new StringWriter();
-        bw = new BufferedWriter(sw);
-        phrase = new Phrase(bw);
-        output = phrase.add("Ceci est une phrase simple. Cette phrase est la deuxième.");
-        assertEquals(2, output);
-        assertEquals("Ceci est une phrase simple.\nCette phrase est la deuxième.", sw.toString());
+		assertEquals(expected, sw.toString());
     }
+    
+    @Test
+    public void testAdd_LineWithPoint() throws IOException {
+        final String expected =INJECTION+ "Ceci est une phrase simple" + ".";
+        // Test avec une ligne qui contient un point
+    	int output = phrase.add("Ceci est une phrase simple" + ".");
+        assertEquals(1, output);
+		assertEquals(expected, sw.toString());
+     }
+    
+    @Test
+    public void testAdd_LineWithExclamation() throws IOException {
+        final String expected =INJECTION+ "Ceci est une phrase simple" + "!";
+        // Test avec une ligne qui contient une exclamation
+        int output = phrase.add("Ceci est une phrase simple" + "!");
+        assertEquals(1, output);
+        assertEquals(expected, sw.toString());
+    }
+
+    @Test
+    public void testAdd_MultipleLines() throws IOException {
+        // Test avec une ligne qui contient plusieurs phrases
+        final String expected =INJECTION+ "Ceci est une phrase simple" + ".\n Cette phrase est la deuxième.";
+        int output = phrase.add("Ceci est une phrase simple. Cette phrase est la deuxième.");
+        assertEquals(2, output);
+		assertEquals(expected, sw.toString());
+    }
+
 }
